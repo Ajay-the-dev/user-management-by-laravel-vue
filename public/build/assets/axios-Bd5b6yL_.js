03c418cb549983609c6eeb2a67f1f4e01f74948c
Ajay-the-dev <ajaychitambaran@gmail.com>
2026-04-26 19:07:22 +0400

    build file created

---
+++ public/build/assets/axios-Bd5b6yL_.js
@@ -0,0 +1,1 @@
+import{D as a}from"./app-CeL8t6a4.js";const t=a.create({baseURL:"http://localhost:8000/api",headers:{"Content-Type":"application/json",Accept:"application/json"}});export{t as a};
---
+++ public/build/assets/feeRouteView-hW4RDVwj.js
@@ -0,0 +1,1 @@
+import{_ as o,g as r,i as t,b as c}from"./app-CeL8t6a4.js";const n={};function s(_,a){const e=t("router-view");return c(),r(e)}const i=o(n,[["render",s]]);export{i as default};
